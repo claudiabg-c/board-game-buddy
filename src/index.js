6c14dd99f4659fc46cf04d10147e2ac43349cb03
Claudia Berrocal <claudia.berrocalgomez@gmail.com>
2024-09-05 13:41:44 +0200

    Add SCSS styles and mixins
    - Created base SCSS file for global styles and resets
    - Added mixins for media queries
    - Addes SCSS for components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import './assets/styles/main.scss';
 import './i18n/i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
