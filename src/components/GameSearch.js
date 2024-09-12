075c6aafa393c6c3c699d9cfc15f09f5ade2f539
Claudia Berrocal <claudia.berrocalgomez@gmail.com>
2024-09-12 23:01:31 +0200

    Enhance board game search with pagination and performance improvements
    - Implemented pagination for search results with page size of 6 to optimize performance by reducing the number of simultaneous API calls.
    - Updated search and pagination controls to only appear after a search is performed and if there are more than 6 results.
    - Fix styles.

--- src/components/GameSearch.js
+++ src/components/GameSearch.js
@@ -1,38 +1,122 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { XMLParser } from 'fast-xml-parser';
 import { useTranslation } from 'react-i18next';
 
-const API_URL = 'https://corsproxy.io/?https://boardgamegeek.com/xmlapi/search';
+const SEARCH_API_URL = 'https://corsproxy.io/?https://boardgamegeek.com/xmlapi/search';
+const GAME_API_URL = 'https://corsproxy.io/?https://boardgamegeek.com/xmlapi/boardgame/';
+const PAGE_SIZE = 6;
 
 function GameSearch({ onGameSelect }) {
     const { t } = useTranslation();
     const [searchTerm, setSearchTerm] = useState('');
+    const [allIds, setAllIds] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
     const [games, setGames] = useState([]);
     const [error, setError] = useState(null);
+    const [searchMessage, setSearchMessage] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [searchPerformed, setSearchPerformed] = useState(false);
+    const [noResultsMessage, setNoResultsMessage] = useState('');
 
-    const handleSearch = async () => {
+    const fetchGameDetails = useCallback(async (ids) => {
         try {
-            const response = await fetch(`${API_URL}?search=${searchTerm}`);
+            const response = await fetch(`${GAME_API_URL}${ids}`);
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
             const text = await response.text();
-            const parser = new XMLParser({
-                ignoreAttributes: false,
-                attributeNamePrefix: "@_"
-            });
+            const parser = new XMLParser({ ignoreAttributes: false, attributeNamePrefix: "@_" });
             const result = parser.parse(text);
+            // Verificar si result.boardgames y result.boardgames.boardgame están definidos
+            if (result.boardgames && result.boardgames.boardgame) {
+                return result.boardgames.boardgame;
+            }
+            return [];
+        } catch (err) {
+            console.error('Error fetching game details:', err);
+            setError(err.message);
+            return [];
+        }
+    }, []);
 
-            const boardgames = result.boardgames.boardgame.map(game => ({
+    const fetchGamesForPage = useCallback(async (page) => {
+        setLoading(true);
+        const startIndex = (page - 1) * PAGE_SIZE;
+        const endIndex = startIndex + PAGE_SIZE;
+        const idsToFetch = allIds.slice(startIndex, endIndex);
+
+        if (idsToFetch.length === 0) {
+            setLoading(false);
+            return;
+        }
+
+        const idsString = idsToFetch.join(',');
+        const gamesData = await fetchGameDetails(idsString);
+
+        if (gamesData.length > 0) {
+            const gamesOnPage = gamesData.map(game => ({
                 id: game['@_objectid'],
                 name: game.name ? game.name['#text'] : 'Unknown',
-                yearpublished: game.yearpublished
+                yearpublished: game.yearpublished,
+                thumbnail: game.thumbnail || null
             }));
-
-            setGames(boardgames);
-        } catch (err) {
-            setError(err.message);
+            setGames(gamesOnPage);
+            setNoResultsMessage('');
+        } else {
+            setGames([]);
+            setSearchMessage('');
+            setNoResultsMessage(`${t('GameSearch.noResults')} "${searchTerm}".`);
         }
+        setLoading(false);
+    }, [allIds, fetchGameDetails, t]);
+
+    const handleSearch = async () => {
+        if (!searchTerm) return;
+
+        setSearchMessage(`${t('GameSearch.searching')} "${searchTerm}".`);
+        setCurrentPage(1);
+        setGames([]);
+        setNoResultsMessage('');
+        setSearchPerformed(true);
+
+        try {
+            const response = await fetch(`${SEARCH_API_URL}?search=${searchTerm}`);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const text = await response.text();
+            const parser = new XMLParser({ ignoreAttributes: false, attributeNamePrefix: "@_" });
+            const result = parser.parse(text);
+
+            if (result.boardgames && result.boardgames.boardgame) {
+                const boardgames = result.boardgames.boardgame.map(game => game['@_objectid']);
+                setAllIds(boardgames);
+                fetchGamesForPage(1);
+            } else {
+                setAllIds([]);
+                setSearchMessage('');
+                setNoResultsMessage(`${t('GameSearch.noResults')} "${searchTerm}".`);
+            }
+        } catch (err) {
+            setSearchMessage(`${err.message}`);
+        }
+    };
+
+    useEffect(() => {
+        if (allIds.length > 0) {
+            fetchGamesForPage(currentPage);
+        }
+    }, [currentPage, allIds, fetchGamesForPage]);
+
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            handleSearch();
+        }
+    };
+
+    const handleGameClick = (game) => {
+        setGames([game]);
+        onGameSelect(game);
     };
 
     return (
@@ -41,21 +125,42 @@ function GameSearch({ onGameSelect }) {
                 type="text"
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
+                onKeyUp={handleKeyPress}
                 placeholder={t('GameSearch.placeholder')}
             />
             <button onClick={handleSearch}>
                 {t('GameSearch.button')}
             </button>
 
-            {error && <p>{t('GameSearch.error', { error })}</p>}
+            {searchMessage && <p>{searchMessage}</p>}
+            {loading && <p>{t('GameSearch.loading')}</p>}
+            {noResultsMessage && <p>{noResultsMessage}</p>}
 
-            <ul>
+            <ul className='search-result'>
                 {games.map((game) => (
-                    <li key={game.id} onClick={() => onGameSelect(game)}>
-                        {game.name} ({game.yearpublished})
+                    <li key={game.id} onClick={() => handleGameClick(game)}>
+                        <p>{game.name} ({game.yearpublished})</p>
+                        {game.thumbnail && <img src={game.thumbnail} alt={game.name} />}
                     </li>
                 ))}
             </ul>
+
+            {searchPerformed && allIds.length > PAGE_SIZE && (
+                <>
+                    <button
+                        disabled={currentPage === 1 || loading}
+                        onClick={() => setCurrentPage(prevPage => prevPage - 1)}
+                    >
+                        {t('GameSearch.prev')}
+                    </button>
+                    <button
+                        disabled={(currentPage * PAGE_SIZE) >= allIds.length || loading}
+                        onClick={() => setCurrentPage(prevPage => prevPage + 1)}
+                    >
+                        {t('GameSearch.next')}
+                    </button>
+                </>
+            )}
         </section>
     );
 }
