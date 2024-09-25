60337189c34b209dc697bbbd36e2ba8b8e37970d
Claudia Berrocal <claudia.berrocalgomez@gmail.com>
2024-09-25 22:31:09 +0200

    Add selected lang diferentiation

--- src/components/LanguageSwitcher.js
+++ src/components/LanguageSwitcher.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 function LanguageSwitcher() {
     const { i18n } = useTranslation();
+    const [selectedLanguage, setSelectedLanguage] = useState(i18n.language);
 
     const changeLanguage = (lng) => {
         i18n.changeLanguage(lng);
+        setSelectedLanguage(lng);
     };
 
     return (
         <div>
-            <button onClick={() => changeLanguage('en')}>English</button>
-            <button onClick={() => changeLanguage('es')}>Español</button>
+            <button onClick={() => changeLanguage('es')} className={selectedLanguage === 'es' ? 'selected' : ''}>
+                Español
+            </button>
+            <button onClick={() => changeLanguage('en')} className={selectedLanguage === 'en' ? 'selected' : ''}>
+                English
+            </button>
         </div>
     );
 }
